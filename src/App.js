696069430440aaf41232fc2879bebfd500c6fcb8
Sean Brown <seanbrown82@hotmail.com>
2022-11-05 12:40:48 +0000

    Feat:Add victory and gameover conditions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { useState } from "react"
 
 function App() {
   const [gameboard, setGameBoard] = useState([[null, null, null], [null, null, null], [null, null, null]])
-  const [turn, setTurn] = useState("x")
+  const [turn, setTurn] = useState("x");
+  const [gameOver, setGameOver] = useState(false)
 
   function handleClick(positionArray) {
 
@@ -16,10 +17,31 @@ function App() {
       const newGameboard = [...gameboard];
       newGameboard[row][column] = turn
       setGameBoard(newGameboard)
-      turn === "x" ? setTurn("o") : setTurn("x")
-      console.log(row, column)
+      turn === "x" ? setTurn("o") : setTurn("x");
+      setGameOver(checkVictory())
 
     }
+
+  }
+  // gameboard[][]
+  function checkVictory() {
+    const [a, b, c, d, e, f, g, h, i] = gameboard.flat();
+    return (
+      compareSquares(a, b, c) ||
+    compareSquares(d, e, f) ||
+    compareSquares(g, h, i) ||
+    compareSquares(a, d, g) ||
+    compareSquares(b, e, h) ||
+    compareSquares(c, f, i) ||
+    compareSquares(a, e, i) ||
+    compareSquares(c, e, g)
+    )
+
+  }
+
+  function compareSquares(a, b, c) {
+
+    return a!== null && a === b && a === c
 
   }
 
